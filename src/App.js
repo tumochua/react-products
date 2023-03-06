325aac3880eb8b48a94b380f2dbcbb0ab1a12b6a
tumochua <94440913+tumochua@users.noreply.github.com>
2023-03-07 01:13:07 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -5,33 +5,34 @@ function App() {
   const [products, setProducts] = useState(null);
 
   useEffect(() => {
-    fetch(" http://localhost:3001/products")
+    fetch(" http://localhost:3001/products/1")
       .then((response) => response.json())
       .then((data) => setProducts(data));
   }, []);
   console.log(products);
 
   return (
-    <div className="wapper">
-      {products &&
-        products.map((product) => {
-          return (
-            <div key={product.id} className="cards">
-              <img src={product.image} alt="" className="image" />
-              <div>{product.name}</div>
-              {/* <div>{product.address}</div> */}
-              <div
-                dangerouslySetInnerHTML={{
-                  __html:
-                    product &&
-                    product.description &&
-                    product.description.replace(/\n/g, "<br>"),
-                }}
-              />
-            </div>
-          );
-        })}
-    </div>
+    <div>{}</div>
+    // <div className="wapper">
+    //   {products &&
+    //     products.map((product) => {
+    //       return (
+    //         <div key={product.id} className="cards">
+    //           <img src={product.image} alt="" className="image" />
+    //           <div>{product.name}</div>
+    //           {/* <div>{product.address}</div> */}
+    //           <div
+    //             dangerouslySetInnerHTML={{
+    //               __html:
+    //                 product &&
+    //                 product.description &&
+    //                 product.description.replace(/\n/g, "<br>"),
+    //             }}
+    //           />
+    //         </div>
+    //       );
+    //     })}
+    // </div>
   );
 }
 
